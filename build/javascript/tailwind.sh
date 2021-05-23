7c8aa94e4f29710facbf0e28d7bf8c5af6ef3860
leuc <d.leucas@outlook.com>
2021-05-23 05:02:37 +0200

    run against current dev

--- build/javascript/tailwind.sh
+++ build/javascript/tailwind.sh
@@ -4,7 +4,6 @@ set -o nounset
 set -o pipefail
 
 cd $(dirname "${BASH_SOURCE[0]}")
-export NODE_ENV=production
-npm install --silent --no-progress --no-audit --no-optional --no-fund 
-node_modules/.bin/tailwind build -c tailwind.config.js -o tailwind.css &>/dev/null
-node_modules/.bin/postcss tailwind.css -o tailwind.min.css 2>/dev/null
+npm install --silent --no-progress --no-audit --no-fund
+NODE_ENV=production node_modules/.bin/tailwind build -c tailwind.config.js -o tailwind.css &>/dev/null
+NODE_ENV=production node_modules/.bin/postcss tailwind.css -o tailwind.min.css 2>/dev/null
