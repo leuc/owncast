7139a6f2ce821e4d45bebaee71e16c07be187b10
leuc <d.leucas@outlook.com>
2021-05-23 04:27:28 +0200

    WIP

--- build/admin/bundleAdmin.sh
+++ build/admin/bundleAdmin.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2059
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
+PKGER=$(command -v pkger || command -v ~/go/bin/pkger) || (echo "Please install go get github.com/markbates/pkger/cmd/pkger" ; exit 1)
+NPM=$(command -v npm) || (echo "Please install NPM" ; exit 1)
+
 INSTALL_TEMP_DIRECTORY="$(mktemp -d)"
-PROJECT_SOURCE_DIR=$(pwd)
-cd $INSTALL_TEMP_DIRECTORY
+PROJECT_SOURCE_DIR=$(git rev-parse --show-toplevel)
 
 shutdown () {
   rm -rf "$PROJECT_SOURCE_DIR/admin"
@@ -15,26 +16,23 @@ shutdown () {
 }
 trap shutdown INT TERM ABRT EXIT
 
-echo "Cloning owncast admin into $INSTALL_TEMP_DIRECTORY..."
-git clone --depth 1 https://github.com/owncast/owncast-admin 2> /dev/null
+cd $INSTALL_TEMP_DIRECTORY
+
+git clone --quiet --depth 1 https://github.com/owncast/owncast-admin
 cd owncast-admin
 
-echo "Installing npm modules for the owncast admin..."
-npm --silent install 2> /dev/null
+$NPM install --silent --no-progress --no-audit --no-fund 
 
-echo "Building owncast admin..."
-rm -rf .next
-(node_modules/.bin/next build && node_modules/.bin/next export) | grep info
+test -d .next && rm -rf .next
 
-echo "Copying admin to project directory..."
-ADMIN_BUILD_DIR=$(pwd)
-cd $PROJECT_SOURCE_DIR
-mkdir -p admin 2> /dev/null
-cd admin
-cp -R ${ADMIN_BUILD_DIR}/out/* .
+node_modules/.bin/next build 1>/dev/null
+node_modules/.bin/next export -s 1>/dev/null
 
-echo "Bundling admin into owncast codebase..."
-~/go/bin/pkger
+test -d "${INSTALL_TEMP_DIRECTORY}/owncast-admin/out"
+test -d "${PROJECT_SOURCE_DIR}/admin" && rm -rf "${PROJECT_SOURCE_DIR}/admin"
+mv "${INSTALL_TEMP_DIRECTORY}/owncast-admin/out" "${PROJECT_SOURCE_DIR}/admin"
+
+cd ${PROJECT_SOURCE_DIR}
+$PKGER
 
 shutdown
-echo "Done."
